791b7bd96c377d0f5d233592a070ed13171f542c
dhatrikollisetty <dhatrikollisetty@gmail.com>
2024-02-11 09:44:41 +0530

    stdunetAddress

---
+++ src/Address.java
@@ -0,0 +1,9 @@
+public class Address{
+    String city;
+    String village;
+
+    public Address(String city, String village) {
+        this.city = city;
+        this.village = village;
+    }
+}
